27331a1c3cc5b599e0ee9be2d190214d5520570b
Peter Krautzberger <p.krautzberger@gmail.com>
2017-04-20 21:33:23 +0200

    fix url

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
     // modified from https://pugjs.org/js/generic.js
 !function r(e,t,n){function u(i,c){if(!t[i]){if(!e[i]){var f="function"==typeof require&&require;if(!c&&f)return f(i,!0);if(o)return o(i,!0);var s=new Error("Cannot find module '"+i+"'");throw s.code="MODULE_NOT_FOUND",s}var a=t[i]={exports:{}};e[i][0].call(a.exports,function(r){var t=e[i][1][r];return u(t||r)},a,a.exports,r,e,t,n)}return t[i].exports}for(var o="function"==typeof require&&require,i=0;i<n.length;i++)u(n[i]);return u}({1:[function(r,e,t){"use strict";var n=r("pug"),u=function(r){if(r&&r.__esModule)return r;var e={};if(null!=r)for(var t in r)Object.prototype.hasOwnProperty.call(r,t)&&(e[t]=r[t]);return e.default=r,e}(n);r("../browser/menu.js"),window.pug=u},{"../browser/menu.js":1,pug:"pug"}]},{},[1]);
 
+let url = window.location.href.substr(0, location.href.lastIndexOf("/") + 1) + '/content.pug'
 
 // load content.pug and render it
 const load = function(){
-    fetch('http://localhost:8080/content.pug', {
+    fetch(url, {
 	method: 'get'
 }).then(function(response) {
 	response.text().then( function(value){
